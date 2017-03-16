a3fa6be91413f91913f6804867d5466fcf9faabf
sguzunov <sguzunov@gmail.com>
2017-03-16 18:41:15 +0200

    Tested AuthenticationService SignOut and SignUpAsync methods.

---
+++ CodeIt/CodeIt.UnitTests/AuthTests/AuthenticationService/SignOutShould.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin;
+
+using NUnit.Framework;
+using Moq;
+
+using AuthService = CodeIt.Web.Auth.AuthenticationService;
+using Microsoft.Owin.Security;
+
+namespace CodeIt.UnitTests.AuthTests.AuthenticationService
+{
+    [TestFixture]
+    public class SignOutShould
+    {
+        [Test]
+        public void CallOwinContext_ToSignOut()
+        {
+            // Arrange
+            var fakeOwinContext = new Mock<IOwinContext>();
+            var fakeAuthManager = new Mock<IAuthenticationManager>();
+            var authService = new AuthService(fakeOwinContext.Object);
+
+            fakeOwinContext.Setup(x => x.Authentication).Returns(fakeAuthManager.Object);
+            fakeAuthManager.Setup(x => x.SignOut()).Verifiable();
+
+            // Act
+            authService.SignOut();
+
+            // Assert
+            fakeAuthManager.Verify(x => x.SignOut(), Times.AtLeastOnce);
+        }
+    }
+}
---
+++ CodeIt/CodeIt.UnitTests/AuthTests/AuthenticationService/SignUpAsyncShould.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+
+using AuthService = CodeIt.Web.Auth.AuthenticationService;
+using Microsoft.Owin;
+using CodeIt.Web.Auth;
+using CodeIt.UnitTests.AuthTests.Mocks;
+using Microsoft.AspNet.Identity;
+using CodeIt.Data.Models;
+
+namespace CodeIt.UnitTests.AuthTests.AuthenticationService
+{
+    [TestFixture]
+    public class SignUpAsyncShould
+    {
+        [Test]
+        public void ThrowsArgumentNullException_WhenUsername_IsNull()
+        {
+            // Arrange
+            var fakeOwinContext = new Mock<IOwinContext>();
+            var authService = new AuthService(fakeOwinContext.Object);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await authService.SignUpAsync(username: null, email: "somemail", password: "123"));
+        }
+
+        [Test]
+        public void ThrowsArgumentException_WhenUsername_IsEmptyString()
+        {
+            // Arrange
+            var fakeOwinContext = new Mock<IOwinContext>();
+            var authService = new AuthService(fakeOwinContext.Object);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(async () => await authService.SignUpAsync(username: "", email: "somemail", password: "123"));
+        }
+
+        [Test]
+        public void ThrowsArgumentNullException_WhenEmail_IsNull()
+        {
+            // Arrange
+            var fakeOwinContext = new Mock<IOwinContext>();
+            var authService = new AuthService(fakeOwinContext.Object);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await authService.SignUpAsync(username: "John", email: null, password: "123"));
+        }
+
+        [Test]
+        public void ThrowsArgumentException_WhenEmail_IsEmptyString()
+        {
+            // Arrange
+            var fakeOwinContext = new Mock<IOwinContext>();
+            var authService = new AuthService(fakeOwinContext.Object);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(async () => await authService.SignUpAsync(username: "John", email: "", password: "123"));
+        }
+
+        [Test]
+        public void ThrowsArgumentNullException_WhenPassword_IsNull()
+        {
+            // Arrange
+            var fakeOwinContext = new Mock<IOwinContext>();
+            var authService = new AuthService(fakeOwinContext.Object);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await authService.SignUpAsync(username: "John", email: "somemail", password: null));
+        }
+
+        [Test]
+        public void ThrowsArgumentException_WhenPassword_IsEmptyString()
+        {
+            // Arrange
+            var fakeOwinContext = new Mock<IOwinContext>();
+            var authService = new AuthService(fakeOwinContext.Object);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(async () => await authService.SignUpAsync(username: "John", email: "somemail", password: ""));
+        }
+
+        [Test]
+        public async Task CallUserManager_CreateAsync()
+        {
+            // Arrange
+            //var fakeUserStore = new Mock<IUserStore<User>>();
+            //var fakeUserManager = new MockUserManager(fakeUserStore.Object);
+            //var fakeOwinContext = new MockOwinContext(fakeUserManager);
+            //var authService = new AuthService(fakeOwinContext);
+
+            //// Act
+            //var creatingResult = await authService.SignUpAsync("username", "somemail", "123");
+
+            //// Assert
+            //Assert.IsTrue(fakeUserManager.IsUserCreated);
+        }
+    }
+}
