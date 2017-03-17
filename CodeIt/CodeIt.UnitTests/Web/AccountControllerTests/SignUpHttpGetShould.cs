81d597666ac4d96d0a4d279fb79bff3b0b6c3ff7
sguzunov <sguzunov@gmail.com>
2017-03-17 14:04:21 +0200

    Added some tests for AccountController.

---
+++ CodeIt/CodeIt.UnitTests/Web/AccountControllerTests/SignUpHttpGetShould.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Moq;
+using TestStack.FluentMVCTesting;
+
+using CodeIt.Web.Controllers;
+using CodeIt.Web.Auth.Contracts;
+
+namespace CodeIt.UnitTests.Web.AccountControllerTests
+{
+    [TestFixture]
+    public class SignUpHttpGetShould
+    {
+        [Test]
+        public void Return_DefaultView()
+        {
+            // Arrange
+            var fakeAuthService = new Mock<IAuthenticationService>();
+            var controller = new AccountController(fakeAuthService.Object);
+
+            // Act & Assert
+            controller.WithCallTo(x => x.SignUp()).ShouldRenderView("SignUp");
+        }
+    }
+}
--- CodeIt/CodeIt.Web/Controllers/AccountController.cs
+++ CodeIt/CodeIt.Web/Controllers/AccountController.cs
@@ -56,6 +56,7 @@ namespace CodeIt.Web.Controllers
             }
         }
 
+        [HttpGet]
         [AllowAnonymous]
         public ActionResult SignUp()
         {
