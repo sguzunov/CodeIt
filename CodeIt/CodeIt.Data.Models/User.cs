93f5bd13b6a7bf2aa625431b4cacaf0a39883920
sguzunov <sguzunov@gmail.com>
2017-03-18 00:18:33 +0200

    User has first name and last name in db model.

--- CodeIt/CodeIt.Data.Models/User.cs
+++ CodeIt/CodeIt.Data.Models/User.cs
@@ -1,14 +1,27 @@
 ﻿using System.Security.Claims;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+
+using CodeIt.Data.Models.Constants;
 
 namespace CodeIt.Data.Models
 {
     public class User : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(Microsoft.AspNet.Identity.UserManager<User> manager)
+        [Required]
+        [MinLength(ValidationConstants.NamesMinLenght)]
+        [MaxLength(ValidationConstants.NamesMaxLenght)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(ValidationConstants.NamesMinLenght)]
+        [MaxLength(ValidationConstants.NamesMaxLenght)]
+        public string LastName { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
