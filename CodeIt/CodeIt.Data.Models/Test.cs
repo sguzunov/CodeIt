4c16b76118736a991ce881917011b9c966f3e621
sguzunov <sguzunov@gmail.com>
2017-03-17 23:07:03 +0200

    Start working on creating challenge.

---
+++ CodeIt/CodeIt.Data.Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeIt.Data.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Input { get; set; }
+
+        [Required]
+        public string Output { get; set; }
+    }
+}
