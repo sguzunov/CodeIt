00068f6a7d0995039b3802663f4346532b5fdb1a
sguzunov <sguzunov@gmail.com>
2017-03-18 18:47:50 +0200

    Up

--- CodeIt/CodeIt.Common/Constants/ValidationConstants.cs
+++ CodeIt/CodeIt.Common/Constants/ValidationConstants.cs
@@ -6,5 +6,8 @@
         public const int ChallengeTitleMinLength = 5;
         public const int ChallengeTitleMaxLength = 20;
         public const int ChallengeDescriptionMinLength = 50;
+
+        public const int UserNamesMinLength = 6;
+        public const int UserNamesMaxLength = 20;
     }
 }
