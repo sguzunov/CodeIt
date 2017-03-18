852e470252ceb9806dde513a8cf55e08fe3339e3
sguzunov <sguzunov@gmail.com>
2017-03-18 15:33:46 +0200

    Introduced AdministrationController allowing only administration access.

---
+++ CodeIt/CodeIt.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+using CodeIt.Common;
+
+namespace CodeIt.Web.Areas.Administration.Controllers
+{
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class AdministrationController : Controller
+    {
+    }
+}
