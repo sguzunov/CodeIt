9f149715b0fc2bc9ca8e1cee8c4be8a933ecce1e
sguzunov <sguzunov@gmail.com>
2017-03-18 19:10:49 +0200

    DbContext added to ninject bindings.

---
+++ CodeIt/CodeIt.Web.Infrastructure/Bindings/DbBindings.cs
@@ -0,0 +1,15 @@
+﻿using Ninject;
+
+using CodeIt.Data.Contracts;
+using CodeIt.Data;
+
+namespace CodeIt.Web.Infrastructure.Bindings
+{
+    public class DbBindings : INinjectBinding
+    {
+        public void Bind(IKernel kernel)
+        {
+            kernel.Bind<ICodeItDbContext>().To<CodeItDbContext>();
+        }
+    }
+}
