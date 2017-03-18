c8313c6065aeb3503d6fb6c036bdfae33b072759
sguzunov <sguzunov@gmail.com>
2017-03-18 14:42:56 +0200

    Admin area structure added.

---
+++ CodeIt/CodeIt.Web/Areas/Administration/AdminAreaRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace CodeIt.Web.Areas.Administration
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Admin";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                name: "Admin",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" });
+        }
+    }
+}
