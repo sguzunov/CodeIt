d9584714de78c075784723f3c86f12f171015a85
sguzunov <sguzunov@gmail.com>
2017-03-20 20:46:34 +0200

    Exposed service for getting all tracks.

--- CodeIt/CodeIt.Services.Data/TracksService.cs
+++ CodeIt/CodeIt.Services.Data/TracksService.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 using CodeIt.Data.Models;
 using CodeIt.Services.Data.Contracts;
+using CodeIt.Data.Contracts;
+
+using Bytes2you.Validation;
 
 namespace CodeIt.Services.Data
 {
     public class TracksService : ITracksService
     {
+        private readonly IEfRepository<Track> tracksRepository;
+
+        public TracksService(IEfRepository<Track> tracksRepository)
+        {
+            Guard.WhenArgument(tracksRepository, nameof(tracksRepository)).IsNull().Throw();
+
+            this.tracksRepository = tracksRepository;
+        }
+
         public IEnumerable<Track> GetAll()
         {
-            throw new NotImplementedException();
+            var tracks = this.tracksRepository.All.ToList();
+            return tracks;
         }
     }
 }
