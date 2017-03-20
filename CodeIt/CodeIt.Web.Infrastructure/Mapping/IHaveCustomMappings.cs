2763e394421452c3075a95645100d92fd555d785
sguzunov <sguzunov@gmail.com>
2017-03-20 20:11:37 +0200

    Automapper seems to be configured.

---
+++ CodeIt/CodeIt.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace CodeIt.Web.Infrastructure.Mapping
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfigurationProvider configuration);
+    }
+}
