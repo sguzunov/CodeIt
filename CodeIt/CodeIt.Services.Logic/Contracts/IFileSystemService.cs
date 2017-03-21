2f427da182ac3758918bdb42179c32f047aaa90c
sguzunov <sguzunov@gmail.com>
2017-03-22 01:27:45 +0200

    Impemented FileSystemService.

---
+++ CodeIt/CodeIt.Services.Logic/Contracts/IFileSystemService.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace CodeIt.Services.Logic.Contracts
+{
+    public interface IFileSystemService
+    {
+        Task SaveFileAsync(string path, byte[] data);
+
+        Task SaveFileAsync(string path, Stream stream);
+    }
+}
