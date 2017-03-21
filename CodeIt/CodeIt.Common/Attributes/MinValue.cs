3cfe631c42092f1d9b4f0991c877854ae8ff3a57
sguzunov <sguzunov@gmail.com>
2017-03-21 16:53:24 +0200

    Added arttribute MinValue..

---
+++ CodeIt/CodeIt.Common/Attributes/MinValue.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeIt.Common.Attributes
+{
+    public class MinValueAttribute : ValidationAttribute
+    {
+        private readonly double minValue;
+
+        public MinValueAttribute(double minValue)
+        {
+            this.minValue = minValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return (double)value <= this.minValue;
+        }
+    }
+}
--- CodeIt/CodeIt.Common/Constants/ValidationConstants.cs
+++ CodeIt/CodeIt.Common/Constants/ValidationConstants.cs
@@ -4,12 +4,16 @@
     {
         // Models
         public const int ChallengeTitleMinLength = 5;
-        public const int ChallengeTitleMaxLength = 20;
+        public const int ChallengeTitleMaxLength = 50;
         public const int ChallengeDescriptionMinLength = 50;
+        public const int ChallengeDescriptionMaxLength = 500;
 
         public const int UserNamesMinLength = 6;
         public const int UserNamesMaxLength = 20;
 
         public const int TrackNameMaxLength = 50;
+
+        public const double ChallengeTimeinMsMin = 1;
+        public const double ChallengeMemotyinMbMin = 0.001;
     }
 }
