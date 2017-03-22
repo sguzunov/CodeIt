a32017c87f2a8b0744a1e464eee36246b29e80b3
sguzunov <sguzunov@gmail.com>
2017-03-22 16:49:43 +0200

    Added utility class for common purposes with files.

---
+++ CodeIt/CodeIt.Services.Logic/Contracts/IFileUnits.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeIt.Services.Logic.Contracts
+{
+    public interface IFileUnits
+    {
+        string ExtractFileName(string fullName);
+
+        string ExtractFileExtension(string fullName);
+    }
+}
---
+++ CodeIt/CodeIt.Services.Logic/FileUnits.cs
@@ -0,0 +1,27 @@
+﻿using Bytes2you.Validation;
+
+using CodeIt.Services.Logic.Contracts;
+
+namespace CodeIt.Services.Logic
+{
+    public class FileUnits : IFileUnits
+    {
+        public string ExtractFileExtension(string fullName)
+        {
+            Guard.WhenArgument(fullName, nameof(fullName)).IsNullOrEmpty().Throw();
+
+            int lastDotIndex = fullName.LastIndexOf('.');
+            string fileName = fullName.Substring(lastDotIndex + 1);
+            return fileName;
+        }
+
+        public string ExtractFileName(string fullName)
+        {
+            Guard.WhenArgument(fullName, nameof(fullName)).IsNullOrEmpty().Throw();
+
+            int lastDotIndex = fullName.LastIndexOf('.');
+            string fileExt = fullName.Substring(0, lastDotIndex);
+            return fileExt;
+        }
+    }
+}
