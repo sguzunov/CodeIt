7929e149d5b53d6b9e5acac906c8d60b04771d30
sguzunov <sguzunov@gmail.com>
2017-03-22 17:18:28 +0200

    Kendo set up.

--- CodeIt/CodeIt.Web/Config/BundleConfig.cs
+++ CodeIt/CodeIt.Web/Config/BundleConfig.cs
@@ -29,6 +29,14 @@ namespace CodeIt.Web.Config
             //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
             //          "~/Scripts/bootstrap.js",
             //          "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/Kendo/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo")
+                .Include(
+                    "~/Scripts/Kendo/kendo.web.min.js",
+                    "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
@@ -36,6 +44,11 @@ namespace CodeIt.Web.Config
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo")
+                .Include(
+                "~/Content/kendo.common.*",
+                "~/Content/kendo.default.*"));
         }
     }
 }
