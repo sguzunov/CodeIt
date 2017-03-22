40d87fe25d133140654b06d57ab4c7ad3fc06377
sguzunov <sguzunov@gmail.com>
2017-03-22 16:50:10 +0200

    FileSystemService moved to server infrastructure.

---
+++ CodeIt/CodeIt.Web.Infrastructure/FileSystem/FileSystemService.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+
+namespace CodeIt.Web.Infrastructure.FileSystem
+{
+    public class FileSystemService : IFileSystemService
+    {
+        public async Task SaveFileAsync(string path, byte[] data)
+        {
+            this.ResolvePath(path);
+            path = HostingEnvironment.MapPath(path);
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                fs.Write(data, 0, data.Length);
+                await fs.WriteAsync(data, 0, data.Length);
+            }
+        }
+
+        public async Task SaveFileAsync(string path, Stream stream)
+        {
+            byte[] bytesInStream = new byte[stream.Length];
+            stream.Read(bytesInStream, 0, bytesInStream.Length);
+            await this.SaveFileAsync(path, bytesInStream);
+        }
+
+        private void ResolvePath(string path)
+        {
+            var dir = Directory.Exists(path);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
---
+++ CodeIt/CodeIt.Web.Infrastructure/FileSystem/IFileSystemService.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace CodeIt.Web.Infrastructure.FileSystem
+{
+    public interface IFileSystemService
+    {
+        Task SaveFileAsync(string path, byte[] data);
+
+        Task SaveFileAsync(string path, Stream stream);
+    }
+}
