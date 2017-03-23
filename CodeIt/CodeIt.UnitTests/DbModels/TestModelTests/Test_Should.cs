9b5e3cdacfb27e308a39414b696e4c0a6c4975dd
sguzunov <sguzunov@gmail.com>
2017-03-23 12:40:27 +0200

    Test db model tested.

---
+++ CodeIt/CodeIt.UnitTests/DbModels/TestModelTests/Test_Should.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using CodeIt.Data.Models;
+
+namespace CodeIt.UnitTests.DbModels.TestModelTests
+{
+    [TestFixture]
+    public class Test_Should
+    {
+        [Test]
+        public void Has_NavigationPropertyOfTypeChallenge()
+        {
+            bool hasNavigationPropChallange = typeof(Test)
+                .GetProperties()
+                .Any(x => x.PropertyType == typeof(Challenge));
+
+            Assert.IsTrue(hasNavigationPropChallange);
+        }
+    }
+}
