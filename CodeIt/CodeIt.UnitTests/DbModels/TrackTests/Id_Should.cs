56b57d779724edbbacdfafa5991ef425ff90b2da
sguzunov <sguzunov@gmail.com>
2017-03-23 12:48:42 +0200

    Track db model tested.

---
+++ CodeIt/CodeIt.UnitTests/DbModels/TrackTests/Id_Should.cs
@@ -0,0 +1,20 @@
+﻿using CodeIt.Data.Models;
+using NUnit.Framework;
+using System;
+
+namespace CodeIt.UnitTests.DbModels.TrackTests
+{
+    [TestFixture]
+    public class Id_Should
+    {
+        [Test]
+        public void Be_OfTypeGuid()
+        {
+            // Arrange
+            var propType = typeof(Track).GetProperty(nameof(Test.Id)).PropertyType;
+
+            // Act & Assert
+            Assert.IsTrue(propType == typeof(Guid));
+        }
+    }
+}
