66fd1b48cf83d6220bfcd25763eb6b707903172f
sguzunov <sguzunov@gmail.com>
2017-03-26 17:04:42 +0300

    Start working on code execution logic.

---
+++ CodeIt/CodeIt.CodeExecution/Models/SubmissionOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeIt.CodeExecution.Models
+{
+    public class SubmissionOptions
+    {
+        public int Language { get; set; }
+
+        public string SourceCode { get; set; }
+    }
+}
